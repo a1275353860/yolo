9293a700f15345f72045ed6791dc43cdcc0282eb
liuwenping <1275353860@qq.com>
2021-03-03 15:31:41 +0800

    feat(初始化): 项目初始化
    项目初始化

---
+++ yolo-web/src/main/java/com/yolo/web/Application.java
@@ -0,0 +1,16 @@
+package com.yolo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: SpringBoot 启动累
+ * @Author: liuwenping
+ * @DateTime: Created in 2021/3/3 下午2:22 周三
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
